716bcbbaa17172ea904f6dcb0d5093f8e079aa50
lzx-UoU <207946879+lzx-UoU@users.noreply.github.com>
2025-05-09 20:00:15 +0800

    Update函数进阶.py

--- 函数进阶.py
+++ 函数进阶.py
@@ -1,147 +1,148 @@
 """
-#闭包函数
-def funa():
+闭包函数
+def active():
 
-    def outer(x):
+  def outer(x):
 
-        def inner(y):
+    def inner(y):
 
-            return x ** 2 + y
-
-        return inner
-
-    fs = []
-
-    for i in range(1,4):
-
-        fs.append(outer(i))
-
-    return fs
-
-f1,f2,f3 = funa()
-
-print(f'{f1(1)},{f2(2)},{f3(3)}')
-
-#nonlocal关键字
-def outer():
-
-    x = 100
-
-    def inner():
-
-        nonlocal x   #在嵌套函数内修改外部函数的局部变量(global是修改全局变量)
-
-        x = 200
-
-    inner()
-
-    print(x)
-
-outer()
-
-#被装饰的函数含多个参数
-def outer(funa):
-
-    def inner(*args,**kw):
-
-        funa(*args,**kw)
-
+      return x ** 3 + y
+    
     return inner
+  
+  arr = []
 
-def funa(*args,**kw):
+  for x in range(1,6):
 
-    print(args)
+    arr.append(outer(x))
 
-    print(kw)
+  return arr
 
-outer(funa)('haha','heihei',name = 'luo',age = 21)
+f1,f2,f3,f4,f5 = active()
+
+print(f"{f1(1)},{f2(2)},{f3(3)},{f4(4)},{f5(5)}")
 
 
-#多个装饰器
-def outer_1(fn):
+nonlocal关键字
+def outer_function():
 
-    def inner_1():
+  num = 10
+  
+  def inner_function():
+  
+    nonlocal num      # 不使用全局变量的情况下，内层函数可以方便的修改外层函数的变量值
+    
+    num += 5
+    
+    return num
+    
+  return inner_function
+  
+ans = outer_function()
 
-        return '我' + fn() + '很晚'
+print(ans())
 
-    return inner_1      #第一个装饰器
 
-def outer_2(fn):
+被装饰的函数含多个参数
+def outer_function(func):
 
-    def inner_2():
+  def inner_function(*args,**kw):
 
-        return '有一天' + fn() + '导致第二天很困'
+    func(*args,**kw)
 
-    return inner_2     #第二个装饰器
+  return inner_function
 
-@outer_2
-@outer_1
+def func(*args,**kw):
+
+  print(args)        # 注意不要把*传入 ('java', 'python', 'javascript')
+  print(kw)          # {'a': 10, 'b': 20, 'c': 30}
+
+outer_function(func)('java','python','javascript',a = 10,b = 20,c = 30)
+
+
+多个装饰器
+def outer_function_1(func):
+
+    def inner_function_1():
+
+        return "python" + func()
+    
+    return  inner_function_1      # 第一个装饰器
+
+def outer_function_2(func):
+
+    def inner_function_2():
+
+        return "javascript" + func()
+
+    return inner_function_2        # 第二个装饰器
+
+@outer_function_1
+@outer_function_2
+# 多个装饰器的装饰过程,距离被装饰函数最近的装饰器先装饰,距离远的装饰器后装饰.
 def func():
 
-    return '学习python知识'
+    return "Vue.js"
 
-print(func())          #多个装饰器的装饰过程，距离被装饰的函数最近的装饰器(outer_1)先装饰，距离远的装饰器(outer_2)后装饰.
+print(func())    # pythonjavascriptVue.js
 
-#装饰器应用
+
+应用装饰器
 import time
 
-def outer(func):
+def outer_function(func):
 
-    def inner(*args,**kw):
+  def inner_function(*args,**kw):
 
-        start = time.time()
+    time_start = time.time()
 
-        res = func(*args,**kw)
+    func(*args,**kw)
 
-        end = time.time()
+    time_end = time.time()
 
-        print(f"执行用时:{end - start:.2f}s")
+    print(f"程序执行用时:{time_end - time_start:.2f}s")
 
-        time.sleep(2)
+    time.sleep(2)
 
-    return inner
+  return inner_function
 
-@outer
+@outer_function
 def download(filename):
 
-    print(f"{filename}下载开始")
+  print(f"{filename}开始下载！")
 
-    time.sleep(5)
+  time.sleep(5)
 
-    print(f"{filename}下载完成")
+  print(f"{filename}下载完成！")
 
-
-
-@outer
+@outer_function
 def upload(filename):
 
-    print(f"{filename}上传开始")
+  print(f"{filename}开始上传！")
 
-    time.sleep(6)
+  time.sleep(5)
 
-    print(f"{filename}上传结束")
+  print(f"{filename}上传完成！")
 
-
-download("mysql.py")
+download("javascript.py")
 upload("python.py")
 
-#取消装饰器的作用
-from functools import wraps
 
+取消装饰器的作用
+from functools import wraps
 import time
 
 def outer(func):
-
-    @wraps(func)    #wraps函数也是一个装饰器，可以保留被装饰之前的函数
+    @wraps(func)      # wraps函数也是装饰器,可以保留被装饰之前的函数
     def inner(*args,**kw):
 
-        start = time.time()
+        time_start = time.time()
 
-        res = func(*args,**kw)
+        func(*args,**kw)
 
-        end = time.time()
+        time_end = time.time()
 
-        print(f"执行时间:{end - start:.2f}s")
+        print(f"执行时间:{time_end - time_start:.2f}s")
 
     return inner
 
@@ -153,8 +154,6 @@ def download(filename):
     time.sleep(5)
 
     print(f"{filename}下载完成")
-
-
 
 @outer
 def upload(filename):
@@ -165,178 +164,126 @@ def upload(filename):
 
     print(f"{filename}上传结束")
 
-
-download.__wrapped__("mysql.py")   #通过__wrapped__属性获得被装饰之前的函数
+# 通过__wrapped__属性获得被装饰之前的函数
+download.__wrapped__("javascript.py") 
 upload.__wrapped__("python.py")
 
-#匿名函数与判断
-#为真结果 if条件 else 为假结果
+
+匿名函数中执行判断
+# 为真结果 if条件 else 为假结果
 comp = lambda a,b : 'a比b小' if a < b  else 'a >= b'
 
-res = [item for item in filter(lambda x:x > 3,[0,1,2,3,4,5,6,7,89])]    # item for item in filter是filter()函数的取值方式
 
-print(res)
-
-#匿名函数的应用
-from functools import reduce    # reduce(func,iterable,initializer)initializer是初始值，即使序列为空，但是有初始值也会返回初始值并不会报错.
-
+匿名函数的应用
+from functools import reduce    # reduce(func,iterable,initializer)initializer是初始值,即使序列为空,但是有初始值也会返回初始值并不会报错.
 res = reduce(lambda x,y:x * y,[1,2,3,4])
-
 print(res)
 
-#映射函数map(func,seq)
+res = [item for item in filter(lambda x:x > 3,[0,1,2,3,4,5,6,7,89])]    # item for item in filter是filter()函数的取值方式
+print(res)
 
+
+映射函数map(func,seq)
 res = map(lambda x:x * 10,[1,2,3,4,5])
-
 print(list(res))
 
+
 可迭代对象与迭代器
+isinstance(object,type)   #  通过该函数确定继承关系,并且能覆盖type()函数判断数据类型的作用
 
-type()         #判断数据类型
+from collections.abc import Iterable
+print(isinstance('123',Iterable))    #  True
 
-isinstance(object,type)   #可以通过该函数确定继承关系，并且能用type()函数做判断的也可以用该函数。
-
-from collections.abc import Iterable   #导入模块
-
-print(isinstance('123',Iterable))    #True
 
 list_1 = [1,2,3]
+list_2 = iter(list_1)     #  iter()调用对象的 __iter__ 方法,并把 __iter__ 方法返回的结果作为自己的返回值
 
-list_2 = iter(list_1) #iter()调用对象的__iter__(),并把__iter__()方法返回的结果作为自己的返回值
+print(next(list_2))   #  1
+print(next(list_2))   #  2
+print(next(list_2))   #  3
 
-print(list_2)         #打印可迭代对象的内存地址
 
-print(next(list_2))   #1
-
-print(next(list_2))   #2
-
-print(next(list_2))   #3
-
-可迭代对象(范围大)与迭代器对象(范围小)的区别
-
-str_1 = "hello python"
-
+可迭代对象(范围大)与迭代器对象(范围小)区别
+对象拥有 __iter__() 方法是可迭代对象,拥有 __iter__ 与 __next__ 方法是迭代器对象.
+dir(obj)      #  查找对象的属性方法
 from collections.abc import Iterable,Iterator
 
-print(isinstance(str_1,Iterable))   #True
+str = "JavaScript"
+print(isinstance(str,Iterable))    #  True  可迭代对象
+print(isinstance(str,Iterator))    #  False   可迭代对象
 
-print(isinstance(str_1,Iterator))   #False
+str = iter(str)                    #  用 iter() 转换为迭代器对象
+print(isinstance(str_1,Iterator))  #  True  迭代器对象
 
-str_1 = iter(str_1)                 #用iter()转换为迭代器对象(Iterator)
 
-print(isinstance(str_1,Iterator))   #True
-
-object拥有__iter__()方法是可迭代对象，拥有__iter__()与__next__()方法是迭代器对象
-
-dir(obj)    #查找对象的属性方法
-
-自定义迭代器类
-
-class MyIterator(object):
+实现自定义迭代器
+class Iterator(object):
 
     def __init__(self):
 
-        self.x = 1
+        self.num = 0
 
     def __iter__(self):
 
-        return self
+        return self         # 实现 __iter__ 方法,返回迭代器对象本身
 
-    def __next__(self):
+    def __next__(self):     # 实现 __next__ 方法
 
-        if self.x >= 15:
+        if self.num >= 15:
 
             raise StopIteration
 
-        else:
+        self.num += 5
 
-            self.x += 1
+        return self.num
 
-            return self.x
+It = Iterator()
 
-my = MyIterator()
-
-for i in my:
-
+for i in It:
+    
     print(i)
+
 
 生成器函数与生成器表达式
 
 生成器表达式与嵌套条件
-
-nums = (x for x in range(21) if x % 2 == 0)   #直接创建一个生成器
-
+nums = (x for x in range(20) if x % 2 == 0)     #  创建生成器
 for num in nums:
-
     print(num)
 
+
 三元表达式与嵌套条件
-
-nums = (x if x % 2 == 0 else 2 * x for x in range(21))
-
+nums = (x if x % 2 == 0 else 2 * x for x in range(20))
 for i in nums:
-
     print(i)
+
 
 多层嵌套
-
 is_prime = lambda x:x > 1 and all(x % i != 0 for i in range(2,x))
-
-res = (x for x in range(21) if is_prime(x))
-
+res = (x for x in range(20) if is_prime(x))
 for i in res:
-
     print(i)
 
-生成器函数
 
+生成器函数
 def func(n):
 
     x = 0
 
     while x < n:
 
-        yield x    # 暂停并返回当前值
+        yield x      # 暂停并返回当前值
 
-        x += 2     # 恢复后从这里继续执行
+        x += 2       # 恢复后从这里继续执行
 
 for i in func(20):
 
     print(i)
 
+
 生成器函数执行过程
+1.暂停和恢复:生成器函数在遇到 yield 时会暂停执行,并返回一个值
+2.记住状态:暂停时,函数会记住当前的变量值和执行位置
+3.继续执行:下次调用时,从上次暂停的位置继续执行
 
-1.暂停和恢复：生成器函数在遇到 yield 时会暂停执行，并返回一个值。
-
-2.记住状态：暂停时，函数会记住当前的变量值和执行位置。
-
-3.继续执行：下次调用时，从上次暂停的位置继续执行。
-
-def func(n):
-
-    x = 1
-
-    while x < n:
-
-        yield x
-
-        x += 1
-
-print(next(func(3)))   #1
-
-print(next(func(3)))   #1
-
-#注意:这两次打印结果一样是因为这本质上是在重新调用函数，所以当遇到yield时返回的值都是一样的，正确方式如下面所示。
-
-f = func(3)
-
-print(next(f))   #1
-
-print(next(f))   #2
 """
-
-
-
-
-
-
